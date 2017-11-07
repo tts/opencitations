8d3951681a91fd979e228f97c711b119e38a66de
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2017-11-07 16:59:39 +0200

    Wrong link

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Data", tabName = "data", icon = icon("dashboard")),
     HTML("<div class='form-group shiny-input-container'><p>Citations by <a href='http://opencitations.net/corpus'>OpenCitations Corpus</a> and <a href=''>Crossref</a> as of 2017-11-07</p></div>"),
-    HTML("<div class='form-group shiny-input-container'><p>R source code of <a href='https://github.com/tts/ooc'>querying data, and building this web app</a></p></div>")
+    HTML("<div class='form-group shiny-input-container'><p>R source code of <a href='https://github.com/tts/opencitations'>querying data, and building this web app</a></p></div>")
     
   ), width = 180
 )
